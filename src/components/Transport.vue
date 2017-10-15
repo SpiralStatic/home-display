bcada29f790a3483e80d8ede5cc3303e1a946c40
Chris Tsoi <chrisjtsoi.work@gmail.com>
2017-10-15 19:37:11 +0100

    Can now add custom routes to filter list

--- src/components/Transport.vue
+++ src/components/Transport.vue
@@ -3,49 +3,90 @@
 		<el-col :span="24">
 			<el-card class="card-style">
 				<div slot="header">
-					<el-row type="flex" class="row-bg" justify="space-between">
+					<el-row type="flex" justify="space-between">
 						<el-col :span="6"></el-col>
 						<el-col :span="6">
-							<div class="grid-content">Transport / Tube</div>
+							<span class="transport-title">Transport / Tube</span>
 						</el-col>
 						<el-col :span="6">
-							<div class="grid-content">
+							<el-row type="flex">
 								<el-select v-model="selectedRoutes" multiple filterable placeholder="Select Routes" id="selected_routes">
-									<el-option v-for="tubeLine in tubeData" :key="tubeLine.id"
-										:label="tubeLine.name"
-										:value="tubeLine.id">
-									</el-option>
+									<el-option-group :label="selectedGroups.tube" v-if="tubeData.length > 0">
+										<el-option v-for="tubeLine in tubeData" :key="tubeLine.id"
+											:label="tubeLine.name"
+											:value="tubeLine.id">
+										</el-option>
+									</el-option-group>
+									<el-option-group :label="selectedGroups.custom" v-if="customData.length > 0">
+										<el-option v-for="customRoute in customData" :key="customRoute.id"
+											:label="customRoute.name"
+											:value="customRoute.id">
+										</el-option>
+									</el-option-group>
 								</el-select>
-							</div>
+								<el-button type="primary" icon="plus" class="add-route-btn" @click="addRouteDialogVisible = true"></el-button>
+							</el-row>
 						</el-col>
 					</el-row>
   				</div>
 				<div v-for="tubeLine in tubeData" :key="tubeLine.id" v-if="selectedRoutes.indexOf(tubeLine.id) !== -1">
 					<el-button-group class="tube-styling">
 						<el-button class="first-item" :style="{ 'background-color': getTubeColor(tubeLine.id) }">{{ tubeLine.name }}</el-button>
-						<el-popover
-							placement="bottom"
-							width="600"
-							popper-class="further-info"
+						<el-popover placement="bottom" width="600" popper-class="further-info"
 							:disabled="tubeLine.lineStatuses[0].statusSeverity === 10"
 							:content="tubeLine.lineStatuses[0].reason">
-							<el-button slot="reference" class="second-item" :style="{ 'background-color': getTubeColor(tubeLine.id)}">{{ tubeLine.lineStatuses[0].reason }}</el-button>
+							<el-button slot="reference" class="second-item" :style="{ 'background-color': getTubeColor(tubeLine.id) }">{{ tubeLine.lineStatuses[0].reason }}</el-button>
 						</el-popover>						
 						<el-button :type="getStatusColor(tubeLine.lineStatuses[0].statusSeverity)" class="third-item">{{ tubeLine.lineStatuses[0].statusSeverityDescription }}</el-button>
 					</el-button-group>
   				</div>
 			</el-card>
 		</el-col>
+
+		<el-dialog title="Add custom route/s" :visible.sync="addRouteDialogVisible" size="large">
+			<el-autocomplete
+				v-model="routeToSearch"
+				:fetch-suggestions="getRouteData"
+				placeholder="Enter a valid route"
+				@select="selectedRoute"
+				:props="routeProperties">
+				<template slot="prepend">New Route:</template>
+			</el-autocomplete>
+
+			<div v-for="(customRoute, key, index) in customData" :key="customRoute.id" id="custom_routes">
+				<el-button-group class="route-styling">
+					<el-button class="first-item" :style="{ 'background-color': getTubeColor(customRoute.modeName) }">{{ capitalise(customRoute.modeName) }}</el-button>	
+					<el-button class="second-item" :style="{ 'background-color': getTubeColor(customRoute.modeName) }">{{ customRoute.name }}</el-button>
+					<el-button class="third-item" icon="delete" @click="customData.splice(index, 1)"></el-button>
+				</el-button-group>
+			</div>
+
+			<span slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="addRouteDialogVisible = false">Confirm</el-button>
+			</span>
+		</el-dialog>
 	</el-row>
 </template>
 
 <script>
+	const debounce = require('debounce');
+
 	export default {
 		name: 'transport',
 		data () {
 			return {
 				tubeData: [],
-				selectedRoutes: []
+				selectedRoutes: [],
+				selectedGroups: {
+					tube: 'Tube Lines',
+					custom: 'Custom'
+				},
+				customData: [],
+				addRouteDialogVisible: false,
+				routeToSearch: '',
+				routeProperties: {
+					label: 'name'
+				}
 			};
 		},
 		methods: {
@@ -83,6 +124,8 @@
 					return 'rgba(0, 152, 212, 0.85)';
 				case 'waterloo-city':
 					return 'rgba(149, 205, 186, 0.85)';
+				default:
+					return 'rgba(208, 26, 30, 0.85)';
 				}
 			},
 			getStatusColor (status) {
@@ -96,6 +139,22 @@
 				case 20:
 					return 'danger';
 				}
+			},
+			getRouteData: debounce(function (searchTerm, callback) {
+				this.$http
+					.get('https://api.tfl.gov.uk/Line/' + searchTerm)
+					.then((response) => {
+						callback(response.data);
+					},
+					(err) => console.log(err));
+			}, 2000),
+			selectedRoute (route) {
+				this.customData.push(route);
+			},
+			capitalise (input) {
+				if (!input) return '';
+				input = input.toString();
+				return input.charAt(0).toUpperCase() + input.slice(1);
 			}
 		},
 		created () {
@@ -108,6 +167,10 @@
 	.transport {
 		margin-bottom: 2em;
 		
+		.transport-title {
+			line-height: 33px;
+		}
+
 		.card-style {
 			background-color: rgba(255, 255, 255, 0.7);
 			.tube-styling {
@@ -141,9 +204,42 @@
 </style>
 
 <style lang="scss">
+	@import '~@/assets/scss/_variables';
+
 	#selected_routes {
-		.el-select__tags {
+		span {
 			display: none;
+		}
+	}
+
+	.el-button--primary.add-route-btn {
+		background-color: $primary-color;
+		border-color: $secondary-color;
+	}
+
+	.further-info {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		font-size: 1.1rem;
+	}
+
+	#custom_routes {
+		margin: 2%;
+
+		.route-styling {
+			width: 100%;
+
+			.first-item {
+				width: 45%;
+			}
+			.second-item {
+				width: 45%;
+			}
+			.third-item {
+				width: 10%;
+			}
 		}
 	}
 </style>
